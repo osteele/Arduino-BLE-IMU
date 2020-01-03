2f8a22bc69e4f55d03667d3ec83a8a54a8a9c4cf
Oliver Steele <steele@osteele.com>
2020-01-03 11:22:40 +0800

    Move UUIDs to header

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ Configures an Arduino to publish BNO055 orientation data over a Bluetooth Low
 Energy (BLE) connection, for use with
 [osteele/imu-tools](https://github.com/osteele/imu-tools).
 
+`src/bt_imu_service.h` defines the service and characteristic IDs.
+
+Characteristic data are transmitted in little-endian order, to match the
+standard GATT profile characteristics such as the heart-rate characteristic.
+
 This is a work in progress.
--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,13 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <HardwareSerial.h>
+#include "bt_imu_service.h"
 
 const char BLE_ADV_NAME[] = "NYUSHIMA IMU";
 
 const char NF_UART_SERVICE_UUID[] = "6E400001-B5A3-F393-E0A9-E50E24DCCA9E";
 const char NF_UART_RX_CHAR_UUID[] = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
 const char NF_UART_TX_CHAR_UUID[] = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
-
-const char IMU_SERVICE_UUID[] = "509B8001-EBE1-4AA5-BC51-11004B78D5CB";
-const char IMU_QUAT_CHAR_UUID[] = "509B8002-EBE1-4AA5-BC51-11004B78D5CB";
 
 const bool SEND_UART_TX_HEARTBEAT = false;
 const int TX_DELAY = 900 / 60;  // 60 fps, with headroom
@@ -21,7 +19,7 @@ BLECharacteristic *txChar;
 BLECharacteristic *rxChar;
 BLECharacteristic *imuQuatChar;
 
-bool deviceConnected = false;
+int deviceConnectedCount = 0;
 bool prevDeviceConnected = false;
 
 unsigned long nextTxTimeMs = 0;
@@ -29,12 +27,12 @@ unsigned long nextTxTimeMs = 0;
 class MyServerCallbacks : public BLEServerCallbacks {
   void onConnect(BLEServer *server) {
     Serial.println("BLE connected");
-    deviceConnected = true;
+    deviceConnectedCount++;
   };
 
   void onDisconnect(BLEServer *server) {
     Serial.println("BLE disconnected");
-    deviceConnected = false;
+    deviceConnectedCount--;
   }
 };
 
@@ -75,9 +73,9 @@ void setup() {
   rxChar->setCallbacks(new UARTRxCallbacks());
 
   // IMU service and charadcteristics
-  BLEService *imuService = bleServer->createService(IMU_SERVICE_UUID);
+  BLEService *imuService = bleServer->createService(BT_IMU_SERVICE_UUID);
   imuQuatChar = imuService->createCharacteristic(
-      IMU_QUAT_CHAR_UUID, BLECharacteristic::PROPERTY_NOTIFY);
+      BT_IMU_QUAT_CHAR_UUID, BLECharacteristic::PROPERTY_NOTIFY);
   imuQuatChar->addDescriptor(new BLE2902());
 
   Serial.println("Starting BLE...");
@@ -86,17 +84,10 @@ void setup() {
   bleServer->getAdvertising()->start();
 }
 
-static void encode(uint8_t *dst, const float *src) {
-  *dst++ = src[3];
-  *dst++ = src[2];
-  *dst++ = src[1];
-  *dst++ = src[0];
-}
-
 void loop() {
   unsigned long now = millis();
   // This test assumes the device is rebooted in less than 49 days
-  if (deviceConnected && now > nextTxTimeMs) {
+  if (deviceConnectedCount > 0 && now > nextTxTimeMs) {
     if (SEND_UART_TX_HEARTBEAT) {
       static char buffer[10];
       int len = snprintf(buffer, sizeof buffer, "%ld\n", now);
@@ -110,23 +101,16 @@ void loop() {
 
     // const float t = now / 100.0;
     const float quat[] = {1, 2, 1000, 2000000};
-    uint8_t buf[sizeof quat];
-    for (int i = 0; i < sizeof quat; i += sizeof *quat) {
-      encode(&buf[i], &quat[i / sizeof *quat]);
-    }
-
-    // imuQuatChar->setValue((uint8_t *)buf, sizeof buf);
     imuQuatChar->setValue((uint8_t *)quat, sizeof quat);
-    // imuQuatChar->setValue((uint8_t *)"ping", 4);
     imuQuatChar->notify();
 
     nextTxTimeMs = now + TX_DELAY;
   }
 
-  if (!deviceConnected && prevDeviceConnected) {
+  if (!deviceConnectedCount && prevDeviceConnected) {
     delay(500);
     Serial.println("Restart BLE advertising");
     bleServer->startAdvertising();
   }
-  prevDeviceConnected = deviceConnected;
+  prevDeviceConnected = deviceConnectedCount > 0;
 }
