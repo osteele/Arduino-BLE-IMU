9864f18b717780444ef6df16dc19b00ce5a70ed6
Oliver Steele <steele@osteele.com>
2020-01-08 16:32:27 +0800

    Set I2C pins

--- src/main.cpp
+++ src/main.cpp
@@ -18,12 +18,27 @@ static const char BLE_ADV_NAME[] = "ESP32 IMU";
 static BLEServiceManager* bleServiceManager;
 
 static BNO055Base* getBNO055() {
-  // Wire.beginTransmission(0x28);
-  // byte error = Wire.endTransmission();
+  Wire.begin(23, 22);
+  // pinMode(22, INPUT_PULLUP);
+  // pinMode(23, INPUT_PULLUP);
+  // Serial.print("I2C scan -> [");
+  // int count = 0;
+  // for (int i = 0; i < 255; i++) {
+  //   Wire.beginTransmission(0x28);
+  //   byte error = Wire.endTransmission();
+  //   if (error == 0) {
+  //     if (count++ > 0) Serial.print(",");
+  //     Serial.print(i);
+  //   }
+  // }
+  // Serial.println("]");
+
   auto bno = new Adafruit_BNO055();
   if (bno->begin()) {
     Serial.println("Connected to BNO055");
-    return new BNO055Adaptor<Adafruit_BNO055>(*new Adafruit_BNO055());
+    auto bno = *new Adafruit_BNO055();
+    bno.printSensorDetails();
+    return new BNO055Adaptor<Adafruit_BNO055>(bno);
   } else {
     Serial.println("Simulating BNO055");
     return new BNO055Dummy();
