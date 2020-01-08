c96cb051962f214ed59d0bd1b004e63ba8ae275c
Oliver Steele <steele@osteele.com>
2020-01-08 10:37:20 +0800

    Set BLE device name immediately on write

--- src/BLEMACAddressService.h
+++ src/BLEMACAddressService.h
@@ -11,8 +11,14 @@ const char BLE_DEVICE_NAME_CHAR_UUID[] = "709F0003-37E3-439E-A338-23F00067988B";
 
 class BLEDeviceNameCallbacks : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic *ch) {
-    std::string value = ch->getValue();
-    Config::getInstance().setBLEDeviceName(value);
+    std::string deviceName = ch->getValue();
+    Config::getInstance().setBLEDeviceName(deviceName);
+    esp_err_t errRc = ::esp_ble_gap_set_device_name(deviceName.c_str());
+    if (errRc != ESP_OK) {
+      log_e("esp_ble_gap_set_device_name: rc=%d", errRc);
+    }
+    ch->setValue((uint8_t *)deviceName.data(), deviceName.length());
+    ch->notify();
   }
 };
 
@@ -28,8 +34,9 @@ class BLE_MACAddressServiceHandler : public BLEServiceHandler {
     macaddressChar->setValue((uint8_t *)macAddress.data(), macAddress.length());
 
     auto *deviceNameChar = bleService_->createCharacteristic(
-        BLE_DEVICE_NAME_CHAR_UUID,
-        BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+        BLE_DEVICE_NAME_CHAR_UUID, BLECharacteristic::PROPERTY_READ |
+                                       BLECharacteristic::PROPERTY_WRITE |
+                                       BLECharacteristic::PROPERTY_NOTIFY);
     std::string deviceName = Config::getInstance().getBLEDeviceName("");
     deviceNameChar->setValue((uint8_t *)deviceName.data(), deviceName.length());
     deviceNameChar->setCallbacks(new BLEDeviceNameCallbacks());
