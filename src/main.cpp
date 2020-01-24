c3abe1cfebb8eff81aa463d40f1634e266c9f8dd
Oliver Steele <steele@osteele.com>
2020-01-24 21:49:10 +0800

    Move BNO055 from the heap

--- src/main.cpp
+++ src/main.cpp
@@ -16,29 +16,15 @@
 static const char BLE_ADV_NAME[] = "ESP32 IMU";
 
 static BLEServiceManager* bleServiceManager;
+static Adafruit_BNO055 bno055;
 
 static BNO055Base* getBNO055() {
   Wire.begin(23, 22);
-  // pinMode(22, INPUT_PULLUP);
-  // pinMode(23, INPUT_PULLUP);
-  // Serial.print("I2C scan -> [");
-  // int count = 0;
-  // for (int i = 0; i < 255; i++) {
-  //   Wire.beginTransmission(0x28);
-  //   byte error = Wire.endTransmission();
-  //   if (error == 0) {
-  //     if (count++ > 0) Serial.print(",");
-  //     Serial.print(i);
-  //   }
-  // }
-  // Serial.println("]");
 
-  auto bno = new Adafruit_BNO055();
-  if (bno->begin()) {
+  if (bno055.begin()) {
     Serial.println("Connected to BNO055");
-    auto bno = *new Adafruit_BNO055();
-    bno.printSensorDetails();
-    return new BNO055Adaptor<Adafruit_BNO055>(bno);
+    bno055.printSensorDetails();
+    return new BNO055Adaptor<Adafruit_BNO055>(bno055);
   } else {
     Serial.println("Simulating BNO055");
     return new BNO055Dummy();
